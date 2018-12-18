15b187740d2b991082222b46a65e8d3f357798b3
Bruno Led <bruno_led@live.com>
2018-12-18 19:43:03 -0200

    Criação da página "Sobre Nós" e Alteração de alguns CSS. Adição da rota do "Sobre Nós"

---
+++ lanclub/resources/views/sobrenos.blade.php
@@ -0,0 +1,51 @@
+@extends('layout.app', ['current' => 'sobre-nos'])
+
+@section('body')
+
+    <div class="container">
+
+        <br>
+        <div class="row justify-content-center">
+            <h1>Sobre Nós</h1>
+        </div>
+        <div class="row justify-content-center">
+            <hr style="width: 25%;">
+        </div>
+
+        <br>
+
+        <br>
+
+        <div class="row justify-content-center">
+            <h2>O que é o LAN Club?</h2>
+        </div>
+
+        <div class="row justify-content-start">
+            <h5>O The LAN Club é um evento em que pessoas se reúnem e jogam jogos de
+            computador juntos. É um projeto voltado a todas as pessoas que têm interesse
+            em jogar e se divertir com jogos de computador com amigos, além de conhecer
+            novas pessoas com o mesmo interesse. É tanto para pessoas que pretendem
+            ter diversão com jogos com seus amigos quanto para pessoas que pretendem
+            ganhar prêmios em torneios de jogos. Em nossos eventos haverá lanches e
+            bebidas. A depender do evento, também será servida comida. Asseguraremos
+            em nossos eventos computadores para todos os participantes, além de internet,
+            energia e um lugar seguro para jogar. Traga apenas você mesmo ou junte com
+                seus amigos.</h5>
+        </div>
+
+        <div class="row justify-content-center">
+            <h2>Quem somos nós?</h2>
+        </div>
+
+        <div class="row justify-content-start">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Mauris ultrices eros in cursus turpis
+            massa tincidunt. Est placerat in egestas erat imperdiet sed. Fringilla urna porttitor
+                rhoncus dolor purus non. Consectetur a erat nam at lectus urna duis.</h5>
+        </div>
+
+    </div>
+<br>
+    <br>
+
+@endsection
