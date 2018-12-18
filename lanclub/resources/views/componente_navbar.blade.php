c26908f638a1412625f3fe4352a5502de1b4ae1a
Bruno Led <bruno_led@live.com>
2018-12-18 19:20:10 -0200

    Primeiro commit

---
+++ lanclub/resources/views/componente_navbar.blade.php
@@ -0,0 +1,63 @@
+<nav class="navbar navbar-expand-lg navbar-dark rounded" style="background-color: #171626">
+    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse"
+            data-target="#navbar" aria-controls="navbar"
+            aria-expanded="false" aria-label="Toggle navigation"></button>
+
+    <div class="collapse navbar-collapse" id="navbar">
+        <a class="navbar-brand" href="#"><img src="{{ asset('logo.png') }}" width="112" height="28"></a>
+        <ul class="nav navbar-nav nav-fill w-100">
+            {{--@guest--}}
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="">{{ __('Login') }}</a>--}}
+                {{--</li>--}}
+                {{--<li class="nav-item">--}}
+                    {{--@if (Route::has('register'))--}}
+                        {{--<a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>--}}
+                    {{--@endif--}}
+                {{--</li>--}}
+            {{--@else--}}
+            @guest
+                <li @if($current=="home")class="nav-item active" @else class="nav-item" @endif>
+                    <a class="nav-link" href="/"><span class="oi oi-home"></span> Home</a>
+                </li>
+                <li @if($current=="sobre_nos")class="nav-item active" @else class="nav-item" @endif>
+                    <a class="nav-link" href="/sobre-nos"><span class="oi oi-question-mark"></span> Sobre Nós</a>
+                </li>
+                <li @if($current=="eventos")class="nav-item active" @else class="nav-item" @endif>
+                    <a class="nav-link" href="/eventos"><span class="oi oi-calendar"></span> Eventos</a>
+                </li>
+                <li @if($current=="fale_conosco")class="nav-item active" @else class="nav-item" @endif>
+                    <a class="nav-link" href="/fale-conosco"><span class="oi oi-chat"></span> Fale Conosco</a>
+                </li>
+                <li @if($current=="logar")class="nav-item active" @else class="nav-item" @endif>
+                    <a class="nav-link" href="/login"><span class="oi oi-person"></span> Logar</a>
+                </li>
+                <li>
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                    </form>
+                </li>
+
+            @endguest
+
+                {{--<li class="nav-item dropdown">--}}
+                    {{--<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+                        {{--{{ Auth::user()->usuario }} <span class="caret"></span>--}}
+                    {{--</a>--}}
+
+                    {{--<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+                        {{--<a class="dropdown-item" href="{{ route('logout') }}"--}}
+                           {{--onclick="event.preventDefault();--}}
+                                                     {{--document.getElementById('logout-form').submit();">--}}
+                            {{--{{ __('Logout') }}--}}
+                        {{--</a>--}}
+
+                        {{--<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+                            {{--@csrf--}}
+                        {{--</form>--}}
+                    {{--</div>--}}
+                {{--</li>--}}
+            {{--@endguest--}}
+        </ul>
+    </div>
+</nav>
---
+++ lanclub/resources/views/layout/app.blade.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{ asset('open-iconic/font/css/open-iconic-bootstrap.css') }}" rel="stylesheet">
+    <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+        body {
+            background-color: #191730;
+        }
+        hr {
+            background-color: #707070;
+        }
+        h5, h4 {
+            color: #707070;
+        }
+    </style>
+</head>
+<body>
+{{--<div class="container">--}}
+    @component('componente_navbar', ["current" => $current])
+    @endcomponent
+    <main role="main">
+        @hasSection('body')
+            @yield('body')
+        @endif
+    </main>
+{{--</div>--}}
+
+<!-- Footer -->
+<div class="container">
+    <footer class="page-footer font-small pt-4">
+
+        <div class="container-fluid text-center text-md-left">
+
+
+            <div class="row">
+
+                <div class="col-md-4 mb-md-0 mb-3">
+
+                    <h5 class="text-uppercase">Saiba Mais</h5>
+
+                    <hr>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Sistema do Franqueado</a>
+                        </li>
+                        <li>
+                            <a href="#!">Sobre Nós</a>
+                        </li>
+                        <li>
+                            <a href="#!">A Equipe</a>
+                        </li>
+                        <li>
+                            <a href="#!">Termos de Uso</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+
+                <div class="col-md-4 mb-md-0 mb-3">
+
+                    <h5 class="text-uppercase">Contatos</h5>
+
+                    <hr>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Seja um Franqueado</a>
+                        </li>
+                        <li>
+                            <a href="#!">Suporte</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="col-md-4 mb-md-0 mb-3">
+
+
+                    <h5 class="text-uppercase">Redes Sociais</h5>
+
+                    <hr>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+
+
+
+    </footer>
+</div>
+
+
+<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+
+@hasSection('javascript')
+    @yield('javascript')
+@endif
+</body>
+</html>
